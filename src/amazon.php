2a90e26aac204bfcd5aeec31a5aac3a15110ed1d
Joe Huss <detain@interserver.net>
2024-12-05 02:36:57 -0500

    update to plugins

--- src/amazon.php
+++ src/amazon.php
@@ -4,7 +4,7 @@
      * Amazon Functionality
      *
      * @author Joe Huss <detain@interserver.net>
-     * @copyright 2019
+     * @copyright 2025
      * @package MyAdmin
      * @category Billing
      */
